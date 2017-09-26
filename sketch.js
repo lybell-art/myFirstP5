79c5217f3f3ed7db18ea885031598f8fdaecc12e
steliviere <32293736+steliviere@users.noreply.github.com>
2017-09-26 17:50:29 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() 
+{ 
+	createCanvas(windowWidth, windowHeight); 
+	background("#ab1300");
+	noStroke();
+}
+function draw() 
+{
+	if (mouseIsPressed) 
+	{
+	var size = random(10, 100);
+	ellipse(mouseX, mouseY, size, size); 
+	}
+	if(keyIsPressed)
+	{
+		if(key==" ")
+		{
+			background("#ab1300");
+		}
+	}
+}
